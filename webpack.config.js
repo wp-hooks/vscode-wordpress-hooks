ad9df25f6d34d6f85a6db4219d2251ebeda7d675
John Blackbourn <john@humanmade.com>
2021-10-12 11:21:28 +0100

    Formatting.

--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,32 @@ const path = require('path');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  target: 'node',
-  entry: './src/extension.ts',
-  output: {
-    path: path.resolve(__dirname, 'out'),
-    filename: 'extension.js',
-    libraryTarget: 'commonjs2',
-    devtoolModuleFilenameTemplate: '../[resource-path]',
-  },
-  devtool: 'source-map',
-  externals: {
-    vscode: 'commonjs vscode',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-    ],
-  },
+	target: 'node',
+	entry: './src/extension.ts',
+	output: {
+		path: path.resolve(__dirname, 'out'),
+		filename: 'extension.js',
+		libraryTarget: 'commonjs2',
+		devtoolModuleFilenameTemplate: '../[resource-path]',
+	},
+	devtool: 'source-map',
+	externals: {
+		vscode: 'commonjs vscode',
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'ts-loader',
+					},
+				],
+			},
+		],
+	},
 };
